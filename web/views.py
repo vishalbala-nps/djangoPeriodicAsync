269a81d19c1aa2e21765192db2250dc0d0ae4992
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-01-21 10:53:03 +0530

    Error Handling Implemented

--- web/views.py
+++ web/views.py
@@ -7,8 +7,11 @@ import json
 # Create your views here.
 
 def index(request):
-   f = open("currentval.txt","r")
-   mjson = json.load(f)
+   try:
+      f = open("currentval.txt","r")
+      mjson = json.load(f)
+   except:
+      return HttpResponse("Waiting for value...")
    f.close()
    print(mjson)
    return render(request,"details.html",{"n1":mjson["number1"],"n2":mjson["number2"],"ans":mjson["sum"],"date":mjson["pubDate"]})
